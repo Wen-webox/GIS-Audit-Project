12444269497ed757618e9a43999e17e2f8612cb3
Wen-webox <hn139168@gmail.com>
2026-04-21 17:07:32 +0800

    Update map_webview.py

--- ui/map_webview.py
+++ ui/map_webview.py
@@ -13,20 +13,18 @@ class MapWebView(Widget):
         self.html_path = os.path.join(os.getcwd(), 'assets', 'www', 'leaflet_map.html')
         self.webview = None
         
-        # 繪製 Kivy 背景 (除錯用)
+        # 繪製 Kivy 背景
         with self.canvas.before:
             Color(0.2, 0.2, 0.2, 1)
             self.rect = Rectangle(size=self.size, pos=self.pos)
         self.bind(size=self._update_native_view_geometry, pos=self._update_native_view_geometry)
 
         if platform == 'android':
-            # 延遲一點點時間執行，確保 Kivy 視圖已經穩定
             Clock.schedule_once(lambda dt: self._init_android_webview(), 1)
         else:
             self._init_windows_mock()
 
     def _update_native_view_geometry(self, *args):
-        """同步 Kivy 元件的位置到 Android 原生 WebView"""
         self.rect.pos = self.pos
         self.rect.size = self.size
         
@@ -34,13 +32,11 @@ class MapWebView(Widget):
             from jnius import autoclass
             from android.runnable import run_on_ui_thread # type: ignore
             
-            # Kivy 的 (0,0) 在左下，Android 的 (0,0) 在左上，需要轉換
             window = autoclass('org.kivy.android.PythonActivity').mActivity.getWindow().getDecorView()
             win_height = window.getHeight()
             
-            # 計算在 Android 座標系中的位置
             x = int(self.x)
-            y = int(win_height - (self.y + self.height)) # 座標系翻轉
+            y = int(win_height - (self.y + self.height))
             w = int(self.width)
             h = int(self.height)
 
@@ -51,6 +47,8 @@ class MapWebView(Widget):
                 params.leftMargin = x
                 params.topMargin = y
                 self.webview.setLayoutParams(params)
+                # 【關鍵修改 1】：每次更新尺寸時，強制把地圖提到最上層，避免被 Kivy 蓋住
+                self.webview.bringToFront()
             
             update_params()
 
@@ -67,7 +65,6 @@ class MapWebView(Widget):
         WebView = autoclass('android.webkit.WebView')
         WebViewClient = autoclass('android.webkit.WebViewClient')
         activity = autoclass('org.kivy.android.PythonActivity').mActivity
-        FrameLayout = autoclass('android.widget.FrameLayout')
 
         @run_on_ui_thread
         def create_webview():
@@ -77,16 +74,14 @@ class MapWebView(Widget):
             self.webview.getSettings().setAllowFileAccess(True)
             self.webview.setWebViewClient(WebViewClient())
             
-            # 載入地圖
             file_url = f"file://{self.html_path}"
             self.webview.loadUrl(file_url)
             
-            # 【關鍵修復】：不要使用 setContentView，而是 addView
-            # 取得 Android 的最底層佈局，然後把 WebView 疊加在 Kivy 指定的位置上
-            parent = activity.findViewById(16908290) # android.R.id.content
+            parent = activity.findViewById(16908290)
             parent.addView(self.webview)
             
-            # 立即更新一次位置
+            # 【關鍵修改 2】：剛建立好地圖時，強制提到最上層
+            self.webview.bringToFront()
             self._update_native_view_geometry()
 
         create_webview()
